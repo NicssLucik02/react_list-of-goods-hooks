7df55be1b1660159669e9ab6a3009ea28b8fee75
nikita.lucenko02@gmail.com <nikitalucenko@MacBook-Air-Nikita.local>
2025-08-09 02:18:41 +0300

    list of goods TS solution

--- src/App.tsx
+++ src/App.tsx
@@ -16,21 +16,23 @@ export const goodsFromServer = [
 ];
 
 export const App: React.FC = () => {
-  const[activeSort, setActiveSort] = useState<string>('all');
-  const[isReversed, setIsReversed] = useState<boolean>(false);
+  const [activeSort, setActiveSort] = useState<string>('all');
+  const [isReversed, setIsReversed] = useState<boolean>(false);
 
-  enum SortStatus{
+  enum SortStatus {
     All = 'all',
     Alphabetical = 'alphabetical',
     Length = 'length',
   }
 
-  function getSort():string[]{
+  function getSort(): string[] {
     const sorted = [...goodsFromServer];
 
-    activeSort === SortStatus.Alphabetical && (sorted.sort((a, b) => a.localeCompare(b)))
-    ||
-    activeSort === SortStatus.Length && (sorted.sort((a, b) => a.length - b.length));
+    if (activeSort === SortStatus.Alphabetical) {
+      sorted.sort((a, b) => a.localeCompare(b));
+    } else if (activeSort === SortStatus.Length) {
+      sorted.sort((a, b) => a.length - b.length);
+    }
 
     if (isReversed) {
       sorted.reverse();
@@ -42,59 +44,66 @@ export const App: React.FC = () => {
   const resetOnClick = () => {
     setActiveSort(SortStatus.All);
     setIsReversed(false);
-  }
+  };
 
-  const sortedGoods = getSort()
+  const sortedGoods = getSort();
 
   return (
     <div className="section content">
       <div className="buttons">
         <button
-        type="button"
-        className={activeSort === SortStatus.Alphabetical
-          ? "button is-info"
-          : "button is-info is-light"
-        }
-        onClick={() => setActiveSort(SortStatus.Alphabetical)}
+          type="button"
+          className={
+            activeSort === SortStatus.Alphabetical
+              ? 'button is-info'
+              : 'button is-info is-light'
+          }
+          onClick={() => setActiveSort(SortStatus.Alphabetical)}
         >
           Sort alphabetically
         </button>
 
-        <button type="button"
-        className={activeSort === SortStatus.Length
-          ? "button is-info"
-          : "button is-info is-light"
-        }
-
-        onClick={() => setActiveSort(SortStatus.Length)}
+        <button
+          type="button"
+          className={
+            activeSort === SortStatus.Length
+              ? 'button is-info'
+              : 'button is-info is-light'
+          }
+          onClick={() => setActiveSort(SortStatus.Length)}
         >
           Sort by length
         </button>
 
         <button
-            type="button"
-            className={isReversed ? "button is-warning" : "button is-warning is-light" }
-            onClick={() => setIsReversed(prev => !prev)}
+          type="button"
+          className={
+            isReversed ? 'button is-warning' : 'button is-warning is-light'
+          }
+          onClick={() => setIsReversed(prev => !prev)}
         >
           Reverse
         </button>
 
-        {(activeSort !== SortStatus.All || isReversed)  && (
-        <button
+        {(activeSort !== SortStatus.All || isReversed) && (
+          <button
             type="button"
             className="button is-danger is-light"
             onClick={resetOnClick}
-         >
-          Reset
-        </button>)}
+          >
+            Reset
+          </button>
+        )}
       </div>
 
       <ul>
         <ul>
           {sortedGoods.map(item => {
             return (
-              <li data-cy="Good" key={item}>{item}</li>
-            )
+              <li data-cy="Good" key={item}>
+                {item}
+              </li>
+            );
           })}
         </ul>
       </ul>
